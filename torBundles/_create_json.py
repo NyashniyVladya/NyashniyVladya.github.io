6ceb6219e79f5fe12daad6429ccdb0bb875da66c
Vladya <NyashniyVladya@users.noreply.github.com>
2025-04-12 23:45:23 +0300

    upd.

--- torBundles/_create_json.py
+++ torBundles/_create_json.py
@@ -6,13 +6,17 @@
 
 import pathlib
 import json
+import requests
 
 
 BASEDIR = pathlib.Path(__file__).parent.resolve(True)
 VERSIONS = BASEDIR.joinpath("versions")
 JSON = BASEDIR.joinpath("versionsSummary.json")
 
-URL = "https://nyashniyvladya.github.io/torBundles/versions/{0}/{1}"
+URL = "https://nyashniyvladya.github.io/{0}/{1}/{{0}}/{{1}}".format(
+    requests.compat.quote(BASEDIR.name),
+    requests.compat.quote(VERSIONS.name)
+)
 
 
 def main():
@@ -23,7 +27,10 @@ def main():
         _version = tuple(map(int, version_dir.name.split('.')))
         _key = '.'.join(map(str, _version))
         for bundle in version_dir.iterdir():
-            _url = URL.format(version_dir.name, bundle.name)
+            _url = URL.format(
+                requests.compat.quote(version_dir.name),
+                requests.compat.quote(bundle.name)
+            )
             result.setdefault(_key, {})[bundle.name] = _url
 
     with JSON.open('w', encoding="utf_8") as _file:
