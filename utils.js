6ae10fb077420193e4b37a7ca30ecc14aa908674
Vladya <NyashniyVladya@users.noreply.github.com>
2025-11-05 01:24:49 +0300

    upd.

---
+++ utils.js
@@ -0,0 +1,12 @@
+﻿// JavaScript Document
+
+async function loadJSON(url) {
+    const response = await fetch(url);
+    let result;
+    if (response.ok) {
+        result = await response.json();
+        return result;
+    };
+    result = await response.text();
+    throw result;
+};
